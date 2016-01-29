842c307818a0f4ba51487a7e722da96c1426aadd
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-01-29 14:23:10 -0300

    Incluir /Lib

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express'),
+    path = require('path'),
     app = express();
 
 
 // HTTP port
 var port = 81;
 
-app.use('/api/internacion', require('../api-internacion/app.js'))
+app
+    .use('/lib', express.static(path.join(__dirname, '../Lib')))
+    .use('/api/internacion', require('../api-internacion/app.js'))
     .use('/app/internacion', require('../app-internacion/app.js'))
     .listen(port, function() {
         console.log('vApp running on port %d', port);
