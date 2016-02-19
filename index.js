7f2a9a8a98e210fde3e3f38244612be72240b28d
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-02-19 11:23:09 -0300

    error handlers

--- index.js
+++ index.js
@@ -2,15 +2,49 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
-
-// HTTP port
+// Config app
 var port = 81;
-
-app
-    .use('/lib', express.static(path.join(__dirname, '../Lib')))
+app.use('/lib', express.static(path.join(__dirname, '../Lib')))
     .use('/auth', require('../auth/app.js'))
     .use('/api/internacion', require('../api-internacion/app.js'))
     .use('/app/internacion', require('../app-internacion/app.js'))
     .listen(port, function() {
         console.log('vApp running on port %d on %s mode', port, app.get('env'));
     });
+
+// Not found: catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+    // Parse err
+    if (!isNaN(err)) {
+        var e = new Error(err == 400 ? "Parámetro incorrecto" : "No encontrado");
+        e.status = err;
+        err = e;
+    } else if (typeof err == "string") {
+        var e = new Error(err);
+        e.status = 400;
+        err = e;
+    }
+
+    // Send HTML or JSON
+    res.status(err.status || 500);
+    var response = {
+        message: err.message,
+        error: (app.get('env') === 'development') ? err : null
+    };
+
+    if (req.accepts('application/json'))
+        res.send(response)
+    else
+        res.render('error', response);
+});
+
+//  View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
